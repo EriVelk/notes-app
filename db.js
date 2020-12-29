2f466ebe6399fa80047158a1c60d7907b7432f9d
EriVelk <erve@live.com.mx>
2020-12-28 20:27:29 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,11 @@
-//Importing the mongoose module
 const mongoose = require('mongoose');
 
-//Set up default mongoose connection
-const dev_db_url = 'process.env.DB' 
+const MONGODB_URI = 'mongodb://localhost/notes-app';
 
-const MONGODB = process.env.MONGODB_URI || dev_db_url;
-
-mongoose.connect(MONGODB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}).then(db => console.log('Database is connect.')).catch(err => console.log('Error: [ ', err));
-
-mongoose.Promise = global.Promise;
-
-const dab = mongoose.connection;
-dab.on('error', console.error.bind(console, 'MongoDB conection error: [ '));
+mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }).then(db => console.log('Database is connect'))
+    .catch(err => console.log('ERROR:[ ', err));
